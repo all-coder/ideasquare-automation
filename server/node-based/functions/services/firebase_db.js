9a26f478aa5b853599a928d9b7cbcfee7c757063
all-coder <niranjanmagesh@gmail.com>
2025-01-27 18:22:17 +0530

    added services

---
+++ server/node-based/functions/services/firebase_db.js
@@ -0,0 +1,11 @@
+//this the firebase db initialization file
+const admin = require("firebase-admin");
+const serviceAccount = require("../permissions.json");
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+});
+
+const db = admin.firestore();
+
+module.exports = db;
