77ee1d817bbe8718606a7f47409529750ecf8b33
all-coder <niranjanmagesh@gmail.com>
2025-01-27 20:08:06 +0530

    renamed filed

--- server/node-based/functions/routes/component_routes.js
+++ server/node-based/functions/routes/component_routes.js
@@ -1,12 +1,11 @@
 // necessary imports
 const express = require("express");
-
+const ComponentModel = require("../models/models");
 //relative imports
-const db = require("../services/firebase_db")
-const { getComponentById, addNewComponent } = require("../services/components");
+const db = require("../services/firebase_db");
+const { getComponentById, addNewComponent,editComponentDetails} = require("../services/component_services");
 
 const routes = express.Router();
-
 
 // routes
 routes.get("/v1", (req, res) => {
@@ -25,18 +24,34 @@ routes.get("/v1/components/:id", async (req, res) => {
 
 routes.post("/v1/components/add", async (req, res) => {
   try {
-    await addNewComponent(
-      db,
+    const newComponent = new ComponentModel(
       req.body.id,
       req.body.name,
       req.body.description,
-      req.body.count
+      req.body.count,
     );
+    await addNewComponent(db, newComponent);
     res.status(200).send("Added New Component Successfully");
   } catch (error) {
     console.error(error);
     res.status(500).send(error);
   }
+});
+
+routes.post("/v1/components/:id/edit",async(req,res)=>{
+    try{
+        const newComponent = new ComponentModel(
+            req.body.id,
+            req.body.name,
+            req.body.description,
+            req.body.count
+        )
+        await editComponentDetails(db,newComponent);
+        res.send(200).send("Component Details Updated Successfully ! ")
+    }catch(error){
+        console.log(error);
+        res.status(500).send("Failed to Update Component Details !")
+    }   
 });
 
 module.exports = routes;
