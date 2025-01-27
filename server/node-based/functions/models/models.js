724590ec1c1dd51656040ef2ab93263dfd6879c3
all-coder <niranjanmagesh@gmail.com>
2025-01-27 19:52:20 +0530

    added Component Model

---
+++ server/node-based/functions/models/models.js
@@ -0,0 +1,11 @@
+class Component{
+    constructor(id,name,description,count){
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.count = count;
+    }
+}
+
+
+module.exports = Component;
