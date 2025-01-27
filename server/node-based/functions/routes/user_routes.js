151d2e7e583b7e2c0379f7eccc2071ff13b91127
all-coder <niranjanmagesh@gmail.com>
2025-01-27 20:32:37 +0530

    set up routing for adding new users

---
+++ server/node-based/functions/routes/user_routes.js
@@ -0,0 +1,30 @@
+// necessary imports
+const express = require("express");
+
+//relative imports
+const db = require("../services/firebase_db");
+const { addNewUser } = require("../services/user_services");
+const { Component, User } = require("../models/models");
+const {
+  getComponentById,
+  addNewComponent,
+  editComponentDetails,
+} = require("../services/component_services");
+
+const routes = express.Router();
+
+routes.post("/v1/user/signup", async (req, res) => {
+  try {
+    const newUser = new User(
+      req.body.name,
+      req.body.roll_no,
+      req.body.club_name,
+      req.body.member_type
+    );
+    await addNewUser(db, newUser);
+    res.status(200).send("User Added Successfully ! ");
+  } catch (error) {
+    console.log(error);
+    res.status(500).send("Failed to Add New User ! ");
+  }
+});
