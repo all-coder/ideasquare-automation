49a0c6b162f7478c6563dcc0b356b5d1a7a33c85
all-coder <niranjanmagesh@gmail.com>
2025-01-27 20:32:59 +0530

    created and implemented addNewUsers()

---
+++ server/node-based/functions/services/user_services.js
@@ -0,0 +1,13 @@
+async function addNewUser(db, user) {
+  await db
+    .collection("users")
+    .doc("/" + user.roll_no + "/")
+    .create({
+      name: user.name,
+      club_name: user.club_name,
+      member_type: user.member_type,
+    });
+}
+
+
+module.exports = {addNewUser};
