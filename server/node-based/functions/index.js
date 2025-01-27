e2603a71bab0fc3f215c02e015e57c46e6a5b31c
all-coder <niranjanmagesh@gmail.com>
2025-01-27 18:22:43 +0530

    modified index.js as per file restructuring

--- server/node-based/functions/index.js
+++ server/node-based/functions/index.js
@@ -1,40 +1,13 @@
+// necessary imports
 const functions = require("firebase-functions");
-var admin = require("firebase-admin");
-var serviceAccount = require("./permissions.json");
-admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-});
-  
+const routes = require("./routes/component_routes");
 
+// initializing express app, and database
 const express = require("express");
-// const cors = require("cors");
-const db = admin.firestore()
+const cors = require("cors");
+
 const app = express();
-// app.use(cors({origin:true}));
 
-
-app.get("/hello-world",(req,res)=>{
-    return res.status(200).send("Hello world");
-});
-
-app.post("/components/add",(req,res)=>{
-    (async ()=>{
-        try{
-            await db.collection("components").doc('/' + req.body.id + '/').create({
-                name:req.body.name,
-                description:req.body.description,
-                count:req.body.count
-
-
-            })
-
-            res.status(200).send();
-        }catch(error)
-        {
-            console.log(error);
-            return res.status(500).send(error);
-        }
-    })();
-});
-
+app.use(cors({ origin: true }));
+app.use(routes)
 exports.app = functions.https.onRequest(app);
