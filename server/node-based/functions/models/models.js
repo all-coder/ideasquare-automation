c3a6a79949890bc0daea36685350c304056cd7e8
all-coder <niranjanmagesh@gmail.com>
2025-01-27 20:07:04 +0530

    added User Model

--- server/node-based/functions/models/models.js
+++ server/node-based/functions/models/models.js
@@ -1,11 +1,19 @@
 class Component{
     constructor(id,name,description,count){
-        this.id = id;
+        this.id = id; // will generate this unique id using uuid on the frontend
         this.name = name;
         this.description = description;
         this.count = count;
     }
 }
 
+class User{
+    constructor(name,roll_no,club_name,member_type){
+        this.name = name;
+        this.roll_no =roll_no; // point of difference
+        this.club_name = club_name;
+        this. member_type = member_type;
+    }
+}
 
 module.exports = Component;
