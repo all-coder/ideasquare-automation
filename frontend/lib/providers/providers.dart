a3c3600237c56c83fcce8bafe153fa223352251e
all-coder <niranjanmagesh@gmail.com>
2025-02-13 22:50:50 +0530

    implemented checkout screen with notifier providers

--- frontend/lib/providers/providers.dart
+++ frontend/lib/providers/providers.dart
@@ -1,9 +1,28 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 // relative imports
 import '../models/dummy.dart';
+import '../models/component.dart';
 
-
-final componentsProvider = Provider((ref){
+// holds the dummy data
+final componentsProvider = Provider((ref) {
   return dummyData;
+});
+
+class CartNotifier extends Notifier<Map<Component, int>> {
+  @override
+  Map<Component, int> build() => {dummyData[0]: 2}; // Initial state
+
+  void addProduct(Component component, int count) {
+    state = {
+      ...state,
+      component:
+          state.containsKey(component) ? state[component]! + count : count,
+    };
+  }
+}
+
+final cartNotifier = NotifierProvider<CartNotifier, Map<Component,int>>(() {
+  return CartNotifier();
 });
