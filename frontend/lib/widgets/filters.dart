32d0412ff88a6c1dff865a57e8314d51a9db29a6
all-coder <niranjanmagesh@gmail.com>
2025-02-13 22:48:28 +0530

    minor modifications

--- frontend/lib/widgets/filters.dart
+++ frontend/lib/widgets/filters.dart
@@ -14,7 +14,7 @@ class Filter extends StatelessWidget {
           borderRadius: BorderRadius.circular(15),
           color: const Color(0xffD9D2D2)),
       margin: const EdgeInsets.fromLTRB(10, 5, 10, 5),
-      child: const Text("*Filters are yet to be decided"),
+      child: const Text("*Filters are yet to be decided*"),
     );
   }
 }
